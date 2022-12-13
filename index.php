f083ac5542cd27ee0b7de9827df7310cae0aa411
PaoloNapoli97 <paolonapoli@live.it>
2022-12-13 18:10:58 +0100

    End

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
+    $text = "Questo è un paragrafo contentente parole scurrili come cazzo e coglione";
+    $badWords = ["cazzo", "coglione"];
+    $censure = "***";
 
+    $newText =  str_replace($badWords,$censure,$text);
+
+    echo $newText;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,5 +17,6 @@
 </head>
 <body>
     <h1>Ciao</h1>
+    <p><?php echo $newText ?></p>
 </body>
 </html>
