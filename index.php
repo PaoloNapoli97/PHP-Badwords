20f157331d92ecb3bdee75bfb1d168fdcf2b7eac
PaoloNapoli97 <paolonapoli@live.it>
2022-12-14 18:04:12 +0100

    Correzione

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-    $text = "Questo è un paragrafo contentente parole scurrili come cazzo e coglione";
-    $badWords = ["cazzo", "coglione"];
+    $text = "Questo è un paragrafo contentente parole scurrili come scemotto e biricchino";
     $censure = "***";
+    $censure_word = isset($_GET['word']) ? $_GET['word'] : ''; 
 
-    $newText =  str_replace($badWords,$censure,$text);
-
-    echo $newText;
+    $newText =  str_replace($censure_word,$censure,$text);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,7 +14,15 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Ciao</h1>
+    <!-- correzione -->
+    <form action="index.php" method="GET">
+        <label for="censure-word">parola da censurare</label>
+        <input name="word" id="censure-word" type="text" placeholder="text">
+        <button type="submit" >Invia</button>
+    </form>
+    <h1>Lunghezza Testo: <?php echo strlen($text); ?></h1>
+    <p><?php echo $text ?></p>
+    <hr>
     <p><?php echo $newText ?></p>
 </body>
 </html>
